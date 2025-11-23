b365a9b5bb4c827116a5db0d8b1773d71b3f8510
gabrielamnss1 <gabriela.m.silva@ba.estudante.senai.br>
2025-11-22 22:41:35 -0300

    Criando função para leitura do arquivo

--- utils/arquivo.py
+++ utils/arquivo.py
@@ -14,3 +14,19 @@ def garantir_diretorio(caminho):
     # então verificamos se diretorio existe e se o caminho não é vazio.
     if diretorio and not os.path.exists(diretorio):
         os.makedirs(diretorio)
+        
+def ler_dados(caminho_arquivo):
+    garantir_diretorio(caminho_arquivo)
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf-8') as f:
+            # Tenta carregar o JSON. Se o arquivo estiver vazio, retorna []
+            conteudo = f.read()
+            if not conteudo:
+                return []
+            return json.loads(conteudo)
+    except FileNotFoundError:
+        # Retorna uma lista vazia se o arquivo não existir (primeira execução)
+        return []
+    except json.JSONDecodeError:
+        # Retorna uma lista vazia se o arquivo estiver corrompido ou vazio
+        return []
